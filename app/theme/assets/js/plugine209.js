fba5a7745e17985a307d7b464c45356972bf334c
yex-games <yexgamesgithub@gmail.com>
2025-01-19 19:43:38 +0500

    Update plugine209.js

--- app/theme/assets/js/plugine209.js
+++ app/theme/assets/js/plugine209.js
@@ -4,29 +4,12 @@
 var Codelug = {};
 
 /**
- * Obfuscated identifiers
- */
-var hiddenSelectors = {
-    gameId: btoa('ingame'), // Encoded ID
-    buttonClass: btoa('in-game-button') // Encoded class
-};
-
-/**
  * Comment
  */
 Codelug.comment = function() {
     if ($('.comments').length > 0) {
         var post_id = $('.comments').attr('data-content');
         var post_type = $('.comments').attr('data-type');
-        
-        // Decode and use the hidden ID and class
-        var gameId = atob(hiddenSelectors.gameId);
-        var buttonClass = atob(hiddenSelectors.buttonClass);
-
-        // Dynamically find elements based on the hidden ID and class
-        var gameElement = $('#' + gameId);
-        var buttonElements = $('.' + buttonClass);
-
         new Comments($('.comments'), {
             ajaxUrl: Base + "/comments",
             content: post_id,
@@ -35,11 +18,17 @@ Codelug.comment = function() {
             replies: true,
             maxDepth: 1,
         });
+    }
 
-        // Example of additional usage with hidden elements
-        gameElement.hide();
-        buttonElements.on('click', function() {
-            console.log('Button clicked!');
-        });
+    // Remove the button with id "inGame"
+    var inGameButton = document.getElementById("inGame");
+    if (inGameButton) {
+        inGameButton.remove();
+        console.log("The in-game button has been removed.");
     }
 };
+
+// Call the Codelug.comment function when DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    Codelug.comment();
+});
